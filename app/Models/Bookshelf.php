f25bab1610ba828de45cc65bf314a74683489a96
unsur-labtif <labtif.unsur@gmail.com>
2026-04-15 10:38:23 +0700

    .

---
+++ app/Models/Bookshelf.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bookshelf extends Model
+{
+    // protected $table = 'bookshelves';
+    // protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'code',
+        'name'
+    ]
+
+    
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,22 +4,33 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable   // users (jamak) -> user (tunggal)
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    // protected $table = 'users';
+    protected $primaryKey = 'npm';
+    public $incrementing = false;
+    protected $keyType = 'int';
+   
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
+
+    // menentukan kolom mana yang bisa diinput user
     protected $fillable = [
-        'name',
+        'npm',
+        'username',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -45,5 +56,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function fullName() : Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->first_name . ' ' . $this->last_name
+        );
     }
 }
